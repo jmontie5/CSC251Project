781355e1d7c25028caea1db146ee66eb20b7fc65
jmontie5 <jmontie5@email.cpcc.edu>
2026-04-13 13:18:23 -0400

    project-3

--- Policy.java
+++ Policy.java
@@ -1,102 +1,48 @@
-// This policy is going to model an insurance policy for one person.
-//It calculates BMI and the total policy price based on specific stuff.
+/**
+ * The Policy class is representing an insurance policy.
+ */
 public class Policy {
-   // Fields are going to be private to ensure the Encapsulation
-   private String policyNumber;
-   private String providerName;
-   private String firstName;
-   private String lastName;
-   private int age;
-   private String smokingStatus; // "smoker" or "non-smoker"
-   private double height; // in inches
-   private double weight; // in pounds
+    private String policyNumber;
+    private String providerName;
+    private PolicyHolder holder; // Aggregation
+    
+    // Static field to track objects
+    private static int policyCount = 0;
 
-   // No-arg constructor 
- 
-   public Policy() {
-      policyNumber = "";
-      providerName = "";
-      firstName = "";
-      lastName = "";
-      age = 0;
-      smokingStatus = "non-smoker";
-      height = 0.0;
-      weight = 0.0;
-   }
+    /**
+     * Parameterized constructor
+     */
+    public Policy(String pNumber, String pProvider, PolicyHolder pCHolder) {
+        policyNumber = pNumber;
+        providerName = pProvider;
+        //Return a copy to avoid security holes
+        this.holder = new PolicyHolder(pCHolder);
+        policyCount++;
+    }
 
-   // Constructor that accepts arguments to initialize the Policy object.
-   // @param pNumber The unique policy number.
-   // @param pProvider The name of the insurance provider.
-   // @param fName The policyholder's first name.
-   // @param lName The policyholder's last name.
-   // @param pAge The policyholder's age.
-   // @param sStatus The smoking status ("smoker" or "non-smoker").
-   // @param pHeight The height in inches.
-   // @param pWeight The weight in pounds.
-   public Policy(String pNumber, String pProvider, String fName, String lName, 
-                 int pAge, String sStatus, double pHeight, double pWeight) {
-      policyNumber = pNumber;
-      providerName = pProvider;
-      firstName = fName;
-      lastName = lName;
-      age = pAge;
-      smokingStatus = sStatus;
-      height = pHeight;
-      weight = pWeight;
-   }
+    /** @return The number of policies created */
+    public static int getPolicyCount() { return policyCount; }
 
-   // Using Setters and Getters
-   
-   public void setPolicyNumber(String pNumber) { policyNumber = pNumber; }
-   public String getPolicyNumber() { return policyNumber; }
+    /**
+     * Calculates total price of the insurance policy
+     * @return The final price
+     */
+    public double calculatePolicyPrice() {
+        double price = 600.0;
+        if (holder.getAge() > 50) price += 75;
+        if (holder.getSmokingStatus().equalsIgnoreCase("smoker")) price += 100;
+        double bmi = holder.calculateBMI();
+        if (bmi > 35) price += (bmi - 35) * 20;
+        return price;
+    }
 
-   public void setProviderName(String pProvider) { providerName = pProvider; }
-   public String getProviderName() { return providerName; }
-
-   public void setFirstName(String fName) { firstName = fName; }
-   public String getFirstName() { return firstName; }
-
-   public void setLastName(String lName) { lastName = lName; }
-   public String getLastName() { return lastName; }
-
-   public void setAge(int pAge) { age = pAge; }
-   public int getAge() { return age; }
-
-   public void setSmokingStatus(String sStatus) { smokingStatus = sStatus; }
-   public String getSmokingStatus() { return smokingStatus; }
-
-   public void setHeight(double pHeight) { height = pHeight; }
-   public double getHeight() { return height; }
-
-   public void setWeight(double pWeight) { weight = pWeight; }
-   public double getWeight() { return weight; }
-
-   //Calculates the Body Mass Index (BMI) of the policyholder.
-   // Using  @return for the calculated BMI value.
-  
-   public double calculateBMI() {
-      if (height == 0) return 0;
-      return (weight * 703) / (height * height);
-   }
-
-   // Calculates total price of the insurance policy
-   // @return The final price of the policy.
-   public double calculatePolicyPrice() {
-      double price = 600.0; 
-      
-      if (age > 50) {
-         price += 75;
-      }
-      
-      if (smokingStatus.equalsIgnoreCase("smoker")) {
-         price += 100;
-      }
-      
-      double bmi = calculateBMI();
-      if (bmi > 35) {
-         price += (bmi - 35) * 20;
-      }
-      
-      return price;
-   }
+    /**
+     * toString method 
+     * @return String representation of the Policy
+     */
+    @Override
+    public String toString() {
+        return String.format("Policy Number: %s\nProvider Name: %s\n%s\nPolicy Price: $%.2f",
+                policyNumber, providerName, holder.toString(), calculatePolicyPrice());
+    }
 }
--- PolicyDemo.java
+++ PolicyDemo.java
@@ -1,17 +1,15 @@
 import java.util.*;
 import java.io.*;
 
-//This PolicyDemo class will be reading the insurance policy data from a file,
-//The data will get stored in an ArrayList and later be displayed
+/**
+ * The PolicyDemo class is going to read the insurance data, and it will create a Policy and a PolicyHolder 
+ * objects.
+ */
 public class PolicyDemo {
    public static void main(String[] args) throws IOException {
-      // Create the ArrayList
       ArrayList<Policy> policyList = new ArrayList<>();
-      
-      // Open the file provided with the policy info
       File file = new File("PolicyInformation.txt");
       
-      // Checking if the file exists before trying to read it
       if (!file.exists()) {
          System.out.println("Unable to find the file: PolicyInformation.txt");
          return;
@@ -19,65 +17,60 @@ public class PolicyDemo {
 
       Scanner inputFile = new Scanner(file);
       
-      // Counters
       int smokerCount = 0;
       int nonSmokerCount = 0;
 
-      // Read the file
       while (inputFile.hasNext()) {
-         // Read the data
+         // Reading Policy Data
          String pNumber = inputFile.nextLine();
          String pProvider = inputFile.nextLine();
+         
+         // Reading PolicyHolder Data
          String fName = inputFile.nextLine();
          String lName = inputFile.nextLine();
-         
          int pAge = inputFile.nextInt();
-         inputFile.nextLine(); 
-         
+         inputFile.nextLine(); // consume newline
          String sStatus = inputFile.nextLine();
          double pHeight = inputFile.nextDouble();
          double pWeight = inputFile.nextDouble();
          
+         // Consume blank line if it exists
+         if (inputFile.hasNextLine()) {
+            inputFile.nextLine(); 
+         }
          if (inputFile.hasNextLine()) {
             inputFile.nextLine(); 
          }
          
-         //Use the constructor that accepts arguments with Encapsulation
-         Policy tempPolicy = new Policy(pNumber, pProvider, fName, lName, pAge, sStatus, pHeight, pWeight);
+         // Class collaboration
+         // Create the PolicyHolder 
+         PolicyHolder holder = new PolicyHolder(fName, lName, pAge, sStatus, pHeight, pWeight);
          
-         // Add the object to the ArrayList
+         // Passing the holder to the Policy constructor
+         Policy tempPolicy = new Policy(pNumber, pProvider, holder);
+         
+         // Addding to list
          policyList.add(tempPolicy);
-      }
-      
-      // Close the file when it has been finished reading
-      inputFile.close();
-
-      //Display the information
-      for (Policy p : policyList) {
-         System.out.println("Policy Number: " + p.getPolicyNumber());
-         System.out.println("Provider Name: " + p.getProviderName());
-         System.out.println("Policyholder's First Name: " + p.getFirstName());
-         System.out.println("Policyholder's Last Name: " + p.getLastName());
-         System.out.println("Policyholder's Age: " + p.getAge());
-         System.out.println("Policyholder's Smoking Status (smoker/non-smoker): " + p.getSmokingStatus());
          
-         // Adding the units height and weight
-         System.out.printf("Policyholder's Height: %.1f inches\n", p.getHeight());
-         System.out.printf("Policyholder's Weight: %.1f pounds\n", p.getWeight());
-         
-         System.out.printf("Policyholder's BMI: %.2f\n", p.calculateBMI());
-         System.out.printf("Policy Price: $%.2f\n", p.calculatePolicyPrice());
-         System.out.println();
-
-         // Count smokers vs non-smokers status
-         if (p.getSmokingStatus().equalsIgnoreCase("smoker")) {
+         // Counting smoking status
+         if (sStatus.equalsIgnoreCase("smoker")) {
             smokerCount++;
          } else {
             nonSmokerCount++;
          }
       }
+      
+      inputFile.close();
 
-      // The final output display
+      // Displaying the information
+      for (Policy p : policyList) {
+         //Implicitly call the toString method
+         System.out.println(p);
+         System.out.println();
+      }
+
+      // Final output display using the static count and the local counters
+      System.out.println("There were " + Policy.getPolicyCount() + " Policy objects created.");
       System.out.println("The number of policies with a smoker is: " + smokerCount);
       System.out.println("The number of policies with a non-smoker is: " + nonSmokerCount);
    }
